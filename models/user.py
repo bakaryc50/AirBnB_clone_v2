0427612b93f5ee7e5c0653a45bef7fed50b19905
Bakaryc50 <bakaryc50@gmail.com>
2022-01-02 15:50:53 -0800

    Bakary commit: user codebase

--- models/user.py
+++ models/user.py
@@ -1,11 +1,26 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel
+from sqlalchemy import Column, String
+from models import storage_type
 
 
 class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    """This class defines a user by various attributes
+    Attributes:
+        email: the email field
+        password: the password
+        first_name: input for first_name
+        last_name: input for last_name
+    """
+    __tablename__ = "users"
+    if storage_type == 'db':
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=False)
+        last_name = Column(String(128), nullable=False)
+    else:
+        email = ''
+        password = ''
+        first_name = ''
+        last_name = ''
