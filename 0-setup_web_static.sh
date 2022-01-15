0d7ab98a67723a072c08bc02e9c236098e3d7194
Bakaryc50 <bakaryc50@gmail.com>
2022-01-15 11:45:37 -0800

    Bakary commit updating script: 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,19 +1,28 @@
 #!/usr/bin/env bash
 #Sets up your web servers for the deployment of web_static
 
-apt -y update
+apt update
 apt -y install nginx
 
-sudo ufw allow "Nginx HTTP"
+ufw allow "Nginx HTTP"
 
-sudo mkdir -p /data/web_static/shared /data/web_static/releases/test
+mkdir -p /data/web_static/shared /data/web_static/releases/test
 
-echo "Holberton School" > /data/web_static/releases/test/index.html
+cat > /data/web_static/releases/test/index.html << 'EOF'
+<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>
+EOF
 
 ln -fns /data/web_static/releases/test /data/web_static/current
 
 chown -hR ubuntu:ubuntu /data
 
 sed -i '/^\tlisten 80 default_server;$/i location /hbnb_static/ { alias /data/web_static/current/; }' /etc/nginx/sites-available/default
+ln -sf '/etc/nginx/sites-available/default' '/etc/nginx/sites-enabled/default'
 
-sudo service nginx restart
+service nginx restart
