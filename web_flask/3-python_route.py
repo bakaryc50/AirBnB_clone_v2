fd13b6ac803c574f0243372e66263cd394286f4f
bakaryc50 <bakaryc50@gmail.com>
2022-02-02 14:14:57 -0800

    Bakary commit: 3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -31,7 +31,7 @@ def c_is_fun(text):
     return "C {}".format(text.replace("_", " "))
 
 
-@app.route("python/", strict_slashes=False)
+@app.route("/python/", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def python_is_cool(text="is cool"):
     """
